5d818b98aa3bce0edbaed6d04a6a044f5d9c8966
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-05-23 15:47:42 +0430

    add route

--- starter/app.js
+++ starter/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
 
 const port = 3000;
-
+// middleware
+app.use(express.json());
 // routes
 app.get('/hello', (req, res) => {
   res.send('Hello my task manager');
 });
-
+// for get all tasks
+app.use('/api/v1/tasks', tasks);
 app.listen(port, () => {
   console.log(`the server run in port : ${port}...`);
 });
