aff878a402de938fc501de3e1ab0bf51ecea4190
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-05-25 17:39:17 +0430

    fix connection

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose');
 const connectionString =
   'mongodb+srv://shirin:RCtWOiGgJH0t2FYg@cluster0.1uhef.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority';
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log('Connected to the db ....'))
-  .catch((err) => console.log(err));
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    // are not supports
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
