4ee944b6e7be9467fa6d0bdfa8fb13fe9459b518
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-05-26 20:28:52 +0430

    fix update and delete in db

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -32,12 +32,34 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send('update task (:');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+    if (!task) {
+      return res.status(404).json({ msg: `NO Task With Id : ${taskId}` });
+    }
+    res.status(200).json({ task });
+    // for delete in frontEnd
+    // res.status(200).send();
+    // res.status(200).json({ task: null, status: 'success' });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete task (:');
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskId }, req.body);
+    if (!task) {
+      return res.status(404).json({ msg: `NO Task With Id : ${taskId}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+  res.send('update task (:');
 };
 
 module.exports = {
